0a506e339a7988b96f2b83b941134e550dffe2f4
Leo Wilson <leo@leomwilson.com>
2020-05-13 20:26:51 -0700

    Move to DDG

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ window.onload = (event) => {
 search.addEventListener('keydown', event => {
     if (event.keyCode === 13) {
         console.log('going')
-        window.location.href = `https://www.google.com/search?q=${search.value}`
+        window.location.href = `https://duckduckgo.com/?q=${search.value}`
     }
 })
